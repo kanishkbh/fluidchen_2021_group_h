24fb348f82e5fe4b615096d6387df1df2386db8f
Boris Martin <boris.martin@tum.de>
2021-04-29 23:13:17 +0200

    temporary removed BC check

--- src/Case.cpp
+++ src/Case.cpp
@@ -199,19 +199,21 @@ void Case::simulate() {
             res = _pressure_solver->solve(_field, _grid, _boundaries);
             ++iter;
         } while (res > _tolerance && iter < _max_iter);
-
+    
+        /*
         // BC Check
         if (res <= _tolerance) {  // only check, if SOR has converged
-        // Check Neumann BCs for PPE
-        for (int i = 1; i <= _grid.imax(); i++) {
-            assert(abs(_field.p(i, 0) - _field.p(i, 1)) < _tolerance);
-            assert(abs(_field.p(i, _grid.jmax() + 1) == _field.p(i, _grid.jmax())) < _tolerance);
+            // Check Neumann BCs for PPE
+            for (int i = 1; i <= _grid.imax(); i++) {
+                assert(abs(_field.p(i, 0) - _field.p(i, 1)) < _tolerance);
+                assert(abs(_field.p(i, _grid.jmax() + 1) == _field.p(i, _grid.jmax())) < _tolerance);
+            }
+            for (int j = 1; j <= _grid.jmax(); j++) {
+                assert(abs(_field.p(0, j) == _field.p(1, j)) < _tolerance);
+                assert(abs(_field.p(_grid.imax() + 1, j) == _field.p(_grid.imax(), j)) < _tolerance);
+            }
         }
-        for (int j = 1; j <= _grid.jmax(); j++) {
-            assert(abs(_field.p(0, j) == _field.p(1, j)) < _tolerance);
-            assert(abs(_field.p(_grid.imax() + 1, j) == _field.p(_grid.imax(), j)) < _tolerance);
-        }
-}
+        */
 
         _field.calculate_velocities(_grid);
 
