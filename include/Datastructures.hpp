7db751f24d32aa25682d827bf7558234140fa28a
Boris Martin <boris.martin@tum.de>
2021-04-29 22:33:27 +0200

    fixed messed up merge history

--- include/Datastructures.hpp
+++ include/Datastructures.hpp
@@ -109,10 +109,10 @@ template <typename T> class Matrix {
     /// Retyurns the absolute maximum in the array.
     T max() const {
         T max_elem = 0; 
-        for(auto j=0;j<jmax;++j){
-            for(auto i=0;i<imax;++i){
-                if(std::abs(_container.at(j*imax+i)) > max_elem){
-                    max_elem = _container.at(j*imax+i);
+        for(auto j=0;j<_jmax;++j){
+            for(auto i=0;i<_imax;++i){
+                if(std::abs(_container.at(j*_imax+i)) > max_elem){
+                    max_elem = _container.at(j*_imax+i);
 
                 }
             }
