6b1dbecf1aa022c2ab8d1665d22f896f90c9ada9
Boris Martin <boris.martin@tum.de>
2021-04-30 22:47:02 +0200

    Fixed (accidentally) removed safety factor

--- src/Fields.cpp
+++ src/Fields.cpp
@@ -94,7 +94,7 @@ double Fields::calculate_dt(Grid &grid) {
         double k1 = (0.5/_nu) * 1/(1/(dx*dx)+(1/dy*dy));
         double k2 = dx/(_U.max() + 1e-8); //Epsilon to ensure no division by 0
         double k3 = dy/(_V.max() + 1e-8);
-        _dt = std::min({k1,k2,k3});
+        _dt = _tau * std::min({k1,k2,k3});
         return _dt;
     }
 }
