c154ff99d365a0b19d9687c66c05c65b3dc68e27
Boris Martin <boris.martin@tum.de>
2021-05-18 18:42:09 +0200

    Fixed wrong BC

--- src/Boundary.cpp
+++ src/Boundary.cpp
@@ -76,20 +76,20 @@ void InflowBoundary::apply(Fields &field) {
 
             switch (border) {
             case border_position::BOTTOM:
-                field.v(i, j - 1) = fluid.v(i, j - 2);
+                field.v(i, j - 1) = _v_in;
                 break;
 
             case border_position::TOP:
-                field.v(i, j) = 0µfield.v(i, j + 1);
+                field.v(i, j) = _v_in;
                 
                 break;
 
             case border_position::LEFT:
-                field.u(i - 1, j) = field.u(i - 2, j);
+                field.u(i - 1, j) = _u_in;
                 break;
 
             case border_position::RIGHT:
-                field.u(i, j) = field.u(i + 1, j);
+                field.u(i, j) = _u_in;
                 break;
 
             default:
@@ -124,27 +124,58 @@ void OutFlowBoundary::apply(Fields &field) {
 
             switch (border) {
             case border_position::BOTTOM:
-                field.v(i, j - 1) = _v_in;
+                field.v(i, j - 1) = _field.v(i, j - 2);
                 // field.u(i, j) = 2 * w - field.u(i, j - 1); // Unchanged tangential speed ?
                 field.p(i, j) = field.p(i, j - 1);
                 break;
 
             case border_position::TOP:
-                field.v(i, j) = _v_in;
+                field.v(i, j) = field.v(i, j + 1);
                 //field.u(i, j) = 2 * w - field.u(i, j + 1); 
                 field.p(i, j) = field.p(i, j + 1);
                 break;
 
             case border_position::LEFT:
-                field.u(i - 1, j) = _u_in;
+                field.u(i - 1, j) = field.u(i - 2, j);
                 //field.v(i, j) = 2 * w - field.v(i - 1, j); 
                 field.p(i, j) = field.p(i - 1, j);
                 break;
 
             case border_position::RIGHT:
-                field.u(i, j) = _u_in;
+                field.u(i, j) = field.u(i + 1, j);
                 //field.v(i, j) = 2 * w - field.v(i + 1, j); // v = - v[right]
                 field.p(i, j) = field.p(i + 1, j);
+                break;
+
+            default:
+                throw std::runtime_error("Unknown border type !");
+                break;
+            }
+        }
+    }
+    for (auto this_cell : _cells) {
+        i = this_cell->i();
+        j = this_cell->j();
+        for (const auto &border : this_cell->borders()) {
+            int i_n = this_cell->neighbour(border)->i();
+            int j_n = this_cell->neighbour(border)->j();
+
+            switch (border) {
+            case border_position::BOTTOM:
+                field.v(i, j - 1) = fluid.v(i, j - 2);
+                break;
+
+            case border_position::TOP:
+                field.v(i, j) = field.v(i, j + 1);
+                
+                break;
+
+            case border_position::LEFT:
+                field.u(i - 1, j) = field.u(i - 2, j);
+                break;
+
+            case border_position::RIGHT:
+                field.u(i, j) = field.u(i + 1, j);
                 break;
 
             default:
