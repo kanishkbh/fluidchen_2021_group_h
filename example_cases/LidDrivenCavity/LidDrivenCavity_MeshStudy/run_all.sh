5bcb5b74b0fcc2e70e3666a1511f0024b3f898cb
Kanishk Bhatia <kanishk.bhatia@tum.de>
2021-05-23 23:00:15 +0200

    added LidDrivenCavity Case files from ws1

---
+++ example_cases/LidDrivenCavity/LidDrivenCavity_MeshStudy/run_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+../../../build/fluidchen dx16.dat
+../../../build/fluidchen dx32.dat
+../../../build/fluidchen dx64.dat
+../../../build/fluidchen dx128.dat
+../../../build/fluidchen dx256.dat
---
+++ example_cases/LidDrivenCavity/LidDrivenCavity_TimeStudy/run_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+../../../build/fluidchen dt_1.0.dat
+../../../build/fluidchen dt_0.5.dat
+../../../build/fluidchen dt_0.1.dat
+../../../build/fluidchen dt_0.05.dat
+../../../build/fluidchen dt_0.005.dat
