b3f9590d9c9f11adf06f249bbb2cb31b865ce3a2
Boris Martin <boris.martin@tum.de>
2021-06-07 21:55:59 +0200

    Timing

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <chrono>
 #include <mpi.h>
 
 #include "Case.hpp"
@@ -10,11 +11,18 @@ int main(int argn, char **args) {
     MPI_Init(nullptr, nullptr);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+
+    
 
     if (argn > 1) {
         std::string file_name{args[1]};
         Case problem(file_name, argn, args);
+        auto t_start = std::chrono::steady_clock::now();
         problem.simulate();
+        auto t_end  = std::chrono::steady_clock::now();
+        if (rank == 0)
+            std::cout << "Time elapsed : " << std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count() << " ms." << std::endl;
 
     } else if (rank == 0) {
         std::cout << "Error: No input file is provided to fluidchen." << std::endl;
